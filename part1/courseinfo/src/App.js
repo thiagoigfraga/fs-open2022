18a97e66aa4592c87872d02f8c22b06042d5511d
Thiago Garcia <thiagogarciafraga99@gmail.com>
2023-01-15 00:30:29 -0300

    Update App.js

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,58 +1,55 @@
-const Header = (props) => <h1>{props.name}</h1>
+const Header = (props) => <h1>{props.name}</h1>;
 
-const Part = ({part}) => {
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
 
-  return(
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={parts[0]}/>
-      <Part part={parts[1]}/>
-      <Part part={parts[2]}/>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </div>
-  )
-}
+  );
+};
 
-const Total = ({parts}) => {
+const Total = ({ parts }) => {
+  const total = parts.reduce((acc, curr) => acc + curr.exercises, 0);
 
-  const total = parts.reduce((acc,curr) => acc + curr.exercises, 0);
-
-  return(
-    <p>Number of exercises {total}</p>
-  )
-} 
+  return <p>Number of exercises {total}</p>;
+};
 
 const App = () => {
-
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
