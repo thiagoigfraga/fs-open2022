ad3f7ae6f01f9f3e819a90d6d0e3251ba8161315
Thiago Garcia <thiagogarciafraga99@gmail.com>
2023-02-24 00:33:09 -0300

    Learning unit tests with jest

---
+++ part4/blog/dummy.test.js
@@ -0,0 +1,10 @@
+const dummy = require('./utils/list_helper').dummy
+
+describe('dummy: ', () => {
+  test('dummy returns one', () => {
+    const blogs = []
+
+    const result = dummy(blogs)
+    expect(result).toBe(1)
+  })
+})
---
+++ part4/blog/utils/list_helper.js
@@ -0,0 +1,14 @@
+const dummy = (blogs) => {
+
+  return 1
+
+}
+
+const totalLikes = (blogs) => {
+
+  return blogs.reduce((acc, curr) => acc + curr.likes, 0)
+}
+
+
+
+module.exports = { dummy,totalLikes }
