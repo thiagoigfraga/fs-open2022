6189d9d757ef6a1c7b8e087657281ae048d3ec8c
Thiago Garcia <thiagogarciafraga99@gmail.com>
2023-02-26 21:30:02 -0300

    backend test configs

---
+++ part3/backend/mongo.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://thiagoigfraga:${password}@cluster0.08vpbv3.mongodb.net/testsNotesApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'I love JavaScript',
+  important: true,
+})
+
+note.save().then((result) => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
